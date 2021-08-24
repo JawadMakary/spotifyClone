966115d8811c8f4a7e57281b8e6851a8f79472c0
jawad <jawadhalbymakary@gmail.com>
2021-08-24 21:26:32 +0300

    first commit

---
+++ src/api/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCXz-irmDEslw_t_nqb8O5tng8qnz79pO4",
+    authDomain: "spotifyclone-caff7.firebaseapp.com",
+    projectId: "spotifyclone-caff7",
+    storageBucket: "spotifyclone-caff7.appspot.com",
+    messagingSenderId: "288109584692",
+    appId: "1:288109584692:web:3efd9687d4abbf624f8561"
+  };
